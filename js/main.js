934aa5af18afb0eaea5ff15887e0b923d8be4d01
klei-wv <cluisenvidiado@gmail.com>
2024-11-22 02:40:40 +0800

    initial commit

--- js/main.js
+++ js/main.js
@@ -2,41 +2,41 @@ const nav = document.querySelector("#nav");
 const navBtn = document.querySelector("#nav-btn");
 const navBtnImg = document.querySelector("#nav-btn-img");
 
-//Preloader
+// Preloader
 function hidePreloader() {
-  const preloader = document.getElementById("preloader");
-  preloader.style.display = "none";
+    const preloader = document.getElementById("preloader");
+    preloader.style.display = "none";
 }
 
 window.addEventListener("load", function () {
-  setTimeout(hidePreloader, 1700);
+    setTimeout(hidePreloader, 1700);
 });
 
-//Hamburger menu
+// Hamburger menu
 navBtn.onclick = () => {
-  if (nav.classList.toggle("open")) {
-    navBtnImg.src = "img/icons/close.svg";
-  } else {
-    navBtnImg.src = "img/icons/open.svg";
-  }
+    if (nav.classList.toggle("open")) {
+        navBtnImg.src = "img/icons/close.svg";
+    } else {
+        navBtnImg.src = "img/icons/open.svg";
+    }
 };
 
-//Sticky header & goToTop button
+// Sticky header & goToTop button
 window.addEventListener("scroll", function () {
-  const header = document.querySelector("#header");
-  const hero = document.querySelector("#home");
-  let triggerHeight = hero.offsetHeight - 170;
+    const header = document.querySelector("#header");
+    const hero = document.querySelector("#home");
+    let triggerHeight = hero.offsetHeight - 170;
 
-  if (window.scrollY > triggerHeight) {
-    header.classList.add("header-sticky");
-    goToTop.classList.add("reveal");
-  } else {
-    header.classList.remove("header-sticky");
-    goToTop.classList.remove("reveal");
-  }
+    if (window.scrollY > triggerHeight) {
+        header.classList.add("header-sticky");
+        goToTop.classList.add("reveal");
+    } else {
+        header.classList.remove("header-sticky");
+        goToTop.classList.remove("reveal");
+    }
 });
 
-//AOS animations settings
+// AOS animations settings
 AOS.init({
-  once: true,
+    once: true,
 });
